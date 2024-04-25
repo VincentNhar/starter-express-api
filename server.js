d9bdfe6f28ea3d1627c9c317f65829b4f89205f4
vince <vincent.calonzo@gmail.com>
2024-04-24 20:18:52 -0400

    uncommentted connection to mongoDB

--- server.js
+++ server.js
@@ -23,17 +23,17 @@ app.use(express.urlencoded());
 // Define a port for the server to listen on
 const PORT = process.env.PORT || 5000
 
-// // MongoDB connection string
-// const DB_CONNECTION_STRING = "mongodb+srv://dbrootadmin:dbpassword@cluster0.o0ag19w.mongodb.net/PlateUpMapSeeds?retryWrites=true&w=majority";
+// MongoDB connection string
+const DB_CONNECTION_STRING = "mongodb+srv://dbrootadmin:dbpassword@cluster0.o0ag19w.mongodb.net/PlateUpMapSeeds?retryWrites=true&w=majority";
 
-// // Connect to MongoDB using Mongoose
-// mongoose.connect(DB_CONNECTION_STRING, {
-// }).then(() => {
-//     console.log("Successfully connected to the MongoDB Server: " + DB_CONNECTION_STRING);    
-// }).catch(err => {
-//     console.log('Could not connect to the MongoDB Server. Exiting now...', err);
-//     process.exit();
-// });
+// Connect to MongoDB using Mongoose
+mongoose.connect(DB_CONNECTION_STRING, {
+}).then(() => {
+    console.log("Successfully connected to the MongoDB Server: " + DB_CONNECTION_STRING);    
+}).catch(err => {
+    console.log('Could not connect to the MongoDB Server. Exiting now...', err);
+    process.exit();
+});
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
