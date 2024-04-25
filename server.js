5ce6e48be1e1b67e67c9c33789d099f719a80763
vince <vincent.calonzo@gmail.com>
2024-04-24 20:16:28 -0400

    .

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ const mongoose = require("mongoose");
 // // Routers
 // const mapRouter = require("./routes/MapRouter");
 
-// // Create an Express application
-// const app = express();
+// Create an Express application
+const app = express();
 
-// // Use CORS middleware to enable Cross-Origin Resource Sharing
-// app.use(cors());
+// Use CORS middleware to enable Cross-Origin Resource Sharing
+app.use(cors());
 
-// // Middleware to parse JSON bodies in incoming requests
-// app.use(express.json({ limit: '10mb' }));
+// Middleware to parse JSON bodies in incoming requests
+app.use(express.json({ limit: '10mb' }));
 
-// // Middleware to parse URL-encoded bodies in incoming requests
-// app.use(express.urlencoded());
+// Middleware to parse URL-encoded bodies in incoming requests
+app.use(express.urlencoded());
 
 // // Routes
 // app.use("/",mapRouter);
 
-// // Define a port for the server to listen on
-// const PORT = process.env.PORT || 5000
+// Define a port for the server to listen on
+const PORT = process.env.PORT || 5000
 
 // // MongoDB connection string
 // const DB_CONNECTION_STRING = "mongodb+srv://dbrootadmin:dbpassword@cluster0.o0ag19w.mongodb.net/PlateUpMapSeeds?retryWrites=true&w=majority";
